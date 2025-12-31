26f7a621a58f73476fc5528b4a6baa79f764ea8a
Ahadinash-Shootorder <ahadinash@shootorder.in>
2025-12-31 18:48:01 +0530

    Meta tag related issues

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
 import { FoundedByAlumni } from '../components/FoundedByAlumni';
@@ -16,36 +17,54 @@ import { Footer } from '../components/Footer';
 
 const Index = () => {
   return (
-    <div className="relative min-h-screen w-full  overflow-x-hidden">
-      {/* <Header />
-      <Hero />
-      <FoundedByAlumni />
-      <ProgramOverview />
-      <CareerStreams />
-      <StudentExperiences />
-      <Testimonials />
-      <ProgramStructure />
-      <ProgramDeliverables />
-      <FoundingTeam />
-      <RealStories />
-      <TrustedSchools />
-      <SampleProjects />
-      <Footer /> */}
-      <Header />
-      <Hero />
-      <FoundedByAlumni />
-      <ProgramOverview />
-      <CareerStreams />
-      <Testimonials />
-      <ProgramStructure />
-      <StudentExperiences />
-      <ProgramDeliverables />
-      <FoundingTeam />
-      <RealStories />
-      <TrustedSchools />
-      <SampleProjects />
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+        <title>GradCircle - Professional Immersion Internship Program</title>
+        <meta name="description" content="Join GradCircle's 4-week virtual internship program for grades 9-12 students. Work with industry mentors from Fortune-500 companies on real-world projects." />
+        <meta name="author" content="GradCircle" />
+
+        <meta property="og:title" content="GradCircle - Professional Immersion Internship Program" />
+        <meta property="og:description" content="Join GradCircle's 4-week virtual internship program for grades 9-12 students. Work with industry mentors from Fortune-500 companies on real-world projects." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://gradcircle.com" />
+        <meta property="og:image" content="https://c.animaapp.com/mjmwrwbp4K97wU/img/gradcircle--white-orange-logo--png-1.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="GradCircle - Professional Immersion Internship Program" />
+        <meta name="twitter:description" content="Join GradCircle's 4-week virtual internship program for grades 9-12 students. Work with industry mentors from Fortune-500 companies on real-world projects." />
+        <meta name="twitter:image" content="https://c.animaapp.com/mjmwrwbp4K97wU/img/gradcircle--white-orange-logo--png-1.png" />
+      </Helmet>
+      <div className="relative min-h-screen w-full  overflow-x-hidden">
+        {/* <Header />
+        <Hero />
+        <FoundedByAlumni />
+        <ProgramOverview />
+        <CareerStreams />
+        <StudentExperiences />
+        <Testimonials />
+        <ProgramStructure />
+        <ProgramDeliverables />
+        <FoundingTeam />
+        <RealStories />
+        <TrustedSchools />
+        <SampleProjects />
+        <Footer /> */}
+        <Header />
+        <Hero />
+        <FoundedByAlumni />
+        <ProgramOverview />
+        <CareerStreams />
+        <Testimonials />
+        <ProgramStructure />
+        <StudentExperiences />
+        <ProgramDeliverables />
+        <FoundingTeam />
+        <RealStories />
+        <TrustedSchools />
+        <SampleProjects />
+        <Footer />
+      </div>
+    </>
   );
 };
 
